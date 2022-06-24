50480c95c21bd42a88a24dc5f75461c9106ef794
Basant Karki <60461905+basant-karki@users.noreply.github.com>
2022-06-25 01:20:21 +0545

    Update yakuza.sh

--- yakuza.sh
+++ yakuza.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 source ~/.profile #refreshment
 
-while wget "https://raw.githubusercontent.com/basant-karki/yakuza/main/domains.txt"; do
+while true; do
 
 finished_alert(){
 	#call the function after finishing the task.
-	clear 
 	echo "All done mr.basant -Jarvis"
 	exit
 }
 
 kxss_for_xss_scanner(){
+	echo "kxss_for_xss_scanner has started..."
 	mkdir kxss
 	cat waybackdata/kxss_raw_urls.txt | kxss > kxss/kxss_suspected_urls.txt
+	clear
 	finished_alert
 }
 
 waybackdata_filters(){
+	echo "wayback_archive_filtering has started..."
 	#mannually-check-http
 	cat waybackdata/wayback-raw-data.txt | grep =http > waybackdata/suspect-urls.txt
 	#gf-patterns
@@ -59,33 +61,46 @@ waybackdata_filters(){
 	#For_KXSS
 	cat waybackdata/wayback-raw-data.txt | grep -iE  = > waybackdata/equals2_urls.txt
 	cat waybackdata/equals2_urls.txt | sed 's/=.*/=/' > waybackdata/waybackurls/kxss_raw_urls.txt
+	clear
 	kxss_for_xss_scanner
 }
 
 
 wayback_archive_hunting(){
+	echo "wayback_archive_hunting has started..."
 	mkdir waybackdata
 	cat subs.txt | gau --threads 2 --o waybackdata/wayback-raw-data.txt
+	clear
 	waybackdata_filters
 }
 
 subdomains_enumeration_and_resolve-m(){
 
-	findomain -f wild.txt --http-status -u wild-subs.txt -q
+	echo "subdomains_enumeration_and_resolving has started......"
+	echo "It can takes time depends upon your domain collection. So Please grab your coffee...."
+	findomain -f wild.txt --http-status -u wild-subs.txt
 	cat single.txt | httprobe > single-live.txt
 	cat single-live.txt > subs.txt
 	cat wild-subs.txt > subs.txt
 	rm -fr single.txt single-live.txt wild.txt wild-temp.txt wild-subs.txt domains.txt
+	clear
 	wayback_archive_hunting
 }
 
 wildcard_domain_management(){
+	echo "wildcard domain management started..."
 	cat domains.txt | grep "*" > wild-temp.txt # Save only the domains which are in the wildcard
 	cat domains.txt | grep -v "*" > single.txt # Save only the single domains
 	sed 's/..//' wild-temp.txt > wild.txt
+	clear
 	subdomains_enumeration_and_resolve-m
 }
 
-wildcard_domain_management # calling domain for domain mamagement
+	if find "domains.txt"; then
+		wildcard_domain_management 	 # calling domain for domain mamagement
+	else
+		wget "http://bugera.000webhostapp.com/domains.txt" -q
+		wildcard_domain_management 	 # calling domain for domain mamagement
+	fi
 	exit
 done
