aa234f11a86699a0c154e655b156cddf746bbf0f
Basant Karki <60461905+basant-karki@users.noreply.github.com>
2022-06-26 18:48:31 +0545

    6/26/2022-v1

--- yakuza.sh
+++ yakuza.sh
@@ -4,20 +4,46 @@ source ~/.profile #refreshment
 while true; do
 
 finished_alert(){
+
 	#call the function after finishing the task.
-	echo "All done mr.basant -Jarvis"
+	clear
+	echo "All done Mr.Karki, you can now check the logs -Jarvis" #Alert admin after completing the task
 	exit
+
 }
 
+
+ffuf_fuzzer(){
+
+	if find "dicc.txt"; then
+		mkdir ffuf-data
+		cat cat subs.txt | xargs -I@ sh -c 'ffuf -w dicc.txt -u @/FUZZ -mc 200 -t 200 -ac -recursion-depth 1' | tee ffuf-data/custom_ffuf_result.txt
+		finished_alert
+
+	else
+		
+		wget "https://bugera.000webhostapp.com/dicc.txt" -q
+		echo "Redirecting......to the same function again 0x0001"
+		sleep 1
+		ffuf_fuzzer #same function redirection
+	fi
+
+}
+
+
 kxss_for_xss_scanner(){
+
+	clear
 	echo "kxss_for_xss_scanner has started..."
 	mkdir kxss
 	cat waybackdata/kxss_raw_urls.txt | kxss > kxss/kxss_suspected_urls.txt
-	clear
 	finished_alert
+
 }
 
 waybackdata_filters(){
+
+	clear
 	echo "wayback_archive_filtering has started..."
 	#mannually-check-http
 	cat waybackdata/wayback-raw-data.txt | grep =http > waybackdata/suspect-urls.txt
@@ -60,47 +86,79 @@ waybackdata_filters(){
 	cat waybackdata/wayback-raw-data.txt | gf xss > waybackdata/xss.txt
 	#For_KXSS
 	cat waybackdata/wayback-raw-data.txt | grep -iE  = > waybackdata/equals2_urls.txt
-	cat waybackdata/equals2_urls.txt | sed 's/=.*/=/' > waybackdata/waybackurls/kxss_raw_urls.txt
+	cat waybackdata/equals2_urls.txt | sed 's/=.*/=/' > waybackdata/kxss_raw_urls.txt
 	clear
 	kxss_for_xss_scanner
+
 }
 
-
 wayback_archive_hunting(){
-	echo "wayback_archive_hunting has started..."
-	mkdir waybackdata
-	cat subs.txt | gau --threads 2 --o waybackdata/wayback-raw-data.txt
-	clear
-	waybackdata_filters
+
+	if find "subs.txt"; then
+
+		clear
+		echo "wayback_archive_hunting has started..."
+		mkdir waybackdata
+		cat subs.txt | gau --threads 5 --o waybackdata/wayback-raw-data.txt
+		clear
+		waybackdata_filters
+
+		else
+
+			clear
+			echo "Didn't find subs.txt for wayback hunting.."
+			exit
+
+	fi
 }
 
 subdomains_enumeration_and_resolve-m(){
 
-	echo "subdomains_enumeration_and_resolving has started......"
-	echo "It can takes time depends upon your domain collection. So Please grab your coffee...."
-	findomain -f wild.txt --http-status -u wild-subs.txt
-	cat single.txt | httprobe > single-live.txt
-	cat single-live.txt > subs.txt
-	cat wild-subs.txt > subs.txt
-	rm -fr single.txt single-live.txt wild.txt wild-temp.txt wild-subs.txt domains.txt
-	clear
-	wayback_archive_hunting
+	if find "subs.txt"; then
+
+		rm -fr single.txt single-live.txt wild.txt wild-temp.txt wild-subs.txt domains.txt
+		wayback_archive_hunting
+
+	else
+
+		clear
+		echo "subdomains_enumeration_and_resolving has started......"
+		echo "It can takes time depends upon your domain collection. So Please grab your coffee...."
+		findomain -f wild.txt --http-status -u wild-subs.txt
+		cat single.txt | httprobe > single-live.txt
+		cat single-live.txt > subs.txt
+		cat wild-subs.txt > subs.txt
+		rm -fr single.txt single-live.txt wild.txt wild-temp.txt wild-subs.txt domains.txt
+		clear
+		wayback_archive_hunting
+
+	fi
 }
 
 wildcard_domain_management(){
+
+	clear
 	echo "wildcard domain management started..."
 	cat domains.txt | grep "*" > wild-temp.txt # Save only the domains which are in the wildcard
 	cat domains.txt | grep -v "*" > single.txt # Save only the single domains
-	sed 's/..//' wild-temp.txt > wild.txt
+	sed 's/..//' wild-temp.txt > wild.txt #regex to remove 2 characters from the front line ex: *. will be remove
 	clear
 	subdomains_enumeration_and_resolve-m
+
 }
 
+main(){
+
 	if find "domains.txt"; then
+		clear
 		wildcard_domain_management 	 # calling domain for domain mamagement
 	else
-		wget "http://bugera.000webhostapp.com/domains.txt" -q
-		wildcard_domain_management 	 # calling domain for domain mamagement
+		wget "http://bugera.000webhostapp.com/domains.txt"
+		wildcard_domain_management 	 # calling domain for domain mamagement after downloading domains.txt
 	fi
+
+}
+
+	main #main function calling
 	exit
 done
